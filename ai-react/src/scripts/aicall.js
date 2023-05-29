6c8aaa217876c15313dbd0ac55be6bd613675b5e
Williams <williams8367@saskpolytech.ca>
2023-05-29 11:34:04 -0600

    all done

--- ai-react/src/scripts/aicall.js
+++ ai-react/src/scripts/aicall.js
@@ -2,12 +2,8 @@ import axios from 'axios';
 
 // Define some OpenAI API whatnots
 const apiURL = 'https://api.openai.com/v1/edits';
-const apiKey = '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'; //API KEY GOES HERE
-// This will be changed
-//var prompt = 'Hello, ChatGPT';
-const maxTokens = 2200;
+const apiKey = ''; //API KEY GOES HERE
 
-//sendCall(prompt);
 // POST call turned into Function
 export async function sendCall(prompt1, language) {
     let payload = "Uh oh";
@@ -15,8 +11,6 @@ export async function sendCall(prompt1, language) {
         "model": "text-davinci-edit-001",
         "input": prompt1.toString(),
         "instruction": "Translate to " + language.toString(),
-        //max_tokens: maxTokens
-        //"temparature": "0.2",
     }, {
         headers: {
             'Authorization': `Bearer ${apiKey}`,
@@ -24,7 +18,7 @@ export async function sendCall(prompt1, language) {
         }
     }).then(response => {
         // This is where the response is spit out
-        //console.log(response.data.choice[0].text);
+
         console.log("Here is the entirety of the response.data:\n" + JSON.stringify(response.data.choices[0].text));
         payload = JSON.stringify(response.data.choices[0].text);
         payload = payload.substring(1, payload.length-3);
